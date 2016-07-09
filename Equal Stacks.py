6334e254054443db490a63f1df188779f4548a1e
ShailBala <ShailBala@users.noreply.github.com>
2016-07-09 20:51:32 +0530

    Sol- World CodeSprint#4 > Equal Stacks-python3

---
+++ Equal Stacks.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import sys
+
+## find length of all 3 stacks
+## store them in l1, l2, l3
+## from the biggest stack, remove the last element,
+## update length and check if l1==l2==l3
+
+def max_length(h1, h2, h3):
+    l1, l2, l3 = 0, 0, 0
+    for each in h1:
+        l1 = l1 + each
+    for each in h2:
+        l2 = l2 + each
+    for each in h3:
+        l3 = l3 + each
+    while l1 !=0 and l2 != 0 and l3 != 0 and (l1!=l2 or l2!=l3):
+        if max(l1, l2, l3) == l1:
+            l1 = l1 - h1[0]
+            h1.pop(0)
+        elif max(l1, l2, l3) == l2:
+            l2 = l2 - h2[0]
+            h2.pop(0)
+        else:
+            l3 = l3 - h3[0]
+            h3.pop(0)
+    else:
+        if l1==l2 and l2==l3:
+            return l1
+        else:
+            return 0
+    
+
+## taking input here
+
+n1,n2,n3 = input().strip().split(' ')
+n1,n2,n3 = [int(n1),int(n2),int(n3)]
+h1 = [int(h1_temp) for h1_temp in input().strip().split(' ')]
+h2 = [int(h2_temp) for h2_temp in input().strip().split(' ')]
+h3 = [int(h3_temp) for h3_temp in input().strip().split(' ')]
+##
+print(max_length(h1, h2, h3))
