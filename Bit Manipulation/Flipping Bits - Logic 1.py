39f6307b3699cca57df9bcfc4ed5ea21a8c9e8da
ShailBala <ShailBala@users.noreply.github.com>
2016-08-05 19:05:50 +0530

    Update Flipping Bits - Logic 1.py

--- Bit Manipulation/Flipping Bits - Logic 1.py
+++ Bit Manipulation/Flipping Bits - Logic 1.py
@@ -4,6 +4,9 @@
 ## Domains:: Algorithns:: Bit Manipulation: Flipping Bits
 ## Logic #1
 
+## See: https://stackoverflow.com/questions/20766813/how-to-convert-signed-to-unsigned-integer-in-python
+## Basically Logic #2, but Python does not have built-in unsigned types
+
 i = int(input())
 while i:
     t = int(input())
