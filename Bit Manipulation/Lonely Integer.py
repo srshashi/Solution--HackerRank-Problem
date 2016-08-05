a3c43a1225a4b14c54f9145748b6f4760e5c6fe2
ShailBala <ShailBala@users.noreply.github.com>
2016-08-05 19:08:35 +0530

    Update Lonely Integer.py

--- Bit Manipulation/Lonely Integer.py
+++ Bit Manipulation/Lonely Integer.py
@@ -3,6 +3,9 @@
 ## Hackerrank
 ## Domains:: Algorithns:: Bit Manipulation: Lonely Integer
 
+## See: https://betterexplained.com/articles/swap-two-variables-using-xor/
+## Hint: Extend it for more than 2 variables
+
 # Head ends here
 def lonelyinteger(b):
     answer = int(b[0])
